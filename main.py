a70463433165c794eadacd416cdac095219174db
ilmir <ilmir531@gmail.com>
2022-06-11 17:28:02 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import time
 import logging
 import requests
 import telegram
+from dotenv import load_dotenv
 
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, tg_chat_id):
+        super().__init__()
+        self.tg_chat_id = tg_chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.tg_chat_id, text=log_entry)
 
 
 def long_polling(token):
+    logger.warning("Бот запущен")
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': token,
@@ -36,8 +49,10 @@ def long_polling(token):
                     is_negative,
                     lesson_url
                 )
-        except requests.exceptions.ReadTimeout:
-            print('сервер не  ответил ')
+        except Exception as err:
+            logger.error("Бот Упал!")
+            logger.error(err, exc_info=True)
+
 
         except requests.exceptions.ConnectionError:
             timeout_seconds = 5
@@ -48,16 +63,22 @@ def telegram_send_message(lesson_title, is_negative, lesson_url):
     positive_text = f'Преподаватель проверил работу!!  \"{lesson_title}\"  {lesson_url} Преподавателю все понравилось, можно приступать к следущему уроку!'
     negative_text = f'Преподаватель проверил работу!!  \"{lesson_title}\"  {lesson_url} К сожалению, в работе нашлись ошибки.'
 
-    bot.send_message(
+    tg_bot.send_message(
         text=negative_text if is_negative else positive_text,
         chat_id=tg_chat_id
     )
 
 
 if __name__ == '__main__':
-    logging.warning('Бот запущен!')
+    load_dotenv()
     devman_token = os.environ['DEVMAN_TOKEN']
     tg_token = os.environ['TG_TOKEN']
     tg_chat_id = os.environ['TG_CHAT_ID']
-    bot = telegram.Bot(tg_token)
+    tg_bot = telegram.Bot(tg_token)
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(tg_bot, tg_chat_id))
+
+    
+
     long_polling(devman_token)
